6b44e4c9f011f8b22623753060dafea43aacd99f
Emracabe <caberemmar@gmail.com>
2023-04-24 12:05:56 +0800

    refactor: change subject to category

--- main.py
+++ main.py
@@ -23,8 +23,10 @@ class App(ttk.Window):
 
 
 
-    # Method to add question inside the questions dictionary
+    # Method when add question button is clicked
     def add_question(self):
+
+        # Get the fields entered by the user
         question = self.question_text.get(1.0, "end-1c")
         option_a = self.option_a_entry.get()
         option_b = self.option_b_entry.get()
@@ -52,7 +54,7 @@ class App(ttk.Window):
             # Update the preview textarea
             self.preview_text.delete('1.0', tk.END)
             for q, opts in self.questions.items():
-                self.preview_text.insert(tk.END, f'{q}?\n')
+                self.preview_text.insert(tk.END, f'{q}\n')
                 self.preview_text.insert(tk.END, f'A. {opts[0]}\n')
                 self.preview_text.insert(tk.END, f'B. {opts[1]}\n')
                 self.preview_text.insert(tk.END, f'C. {opts[2]}\n')
@@ -63,15 +65,15 @@ class App(ttk.Window):
 
 
 
-    # Method when the save button is clicked
+    # Method when the save questions button is clicked
     def save_questions(self):
         path = os.path.join(
-            os.getcwd(), f"question_bank\\{self.subject_combobox.get()}.json")
+            os.getcwd(), f"question_bank\\{self.category_combobox.get()}.json")
 
         # Ask confirmatiion to save the questions
         confirm_to_save = messagebox.askyesno(
             "Confirm Save", 
-            f"Are you sure you want to save the new {self.subject_combobox.get()} questions?")
+            f"Are you sure you want to save the new {self.category_combobox.get()} questions?")
         
         # IF yes, proceed to adding the questions
         if confirm_to_save:
@@ -97,7 +99,7 @@ class App(ttk.Window):
 
 
 
-    # Method to append questions in an existing JSON subject question file
+    # Method to append questions in an existing JSON category question file
     def append_json(self, path):
 
         # Read the existing JSON file
@@ -124,7 +126,7 @@ class App(ttk.Window):
 
 
 
-    # Method to write questions in a JSON subject question file
+    # Method to write questions in a JSON category question file
     def write_json(self, path):
         # print(self.questions.items())
         with open(path, 'w+') as f:
@@ -164,17 +166,17 @@ class App(ttk.Window):
         self.preview_text = ttk.ScrolledText(preview_frame, font=('Arial', 11))
         self.preview_text.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        # Create Subject Combobox
-        self.selected_subject = tk.StringVar()
+        # Create category Combobox
+        self.selected_category = tk.StringVar()
 
-        subject_label = ttk.Label(fields_frame, text='Subjects:')
-        subject_label.pack(side=tk.TOP, padx=10, pady=10)
+        category_label = ttk.Label(fields_frame, text='Categories:')
+        category_label.pack(side=tk.TOP, padx=10, pady=10)
 
-        self.subject_combobox = ttk.Combobox(
-            fields_frame, textvariable=self.selected_subject, state='readonly')
-        self.subject_combobox['values'] = ('ESAS', 'Mathematics', 'EEPS')
-        self.subject_combobox.current(0)
-        self.subject_combobox.pack(side=tk.TOP, padx=10, pady=3)
+        self.category_combobox = ttk.Combobox(
+            fields_frame, textvariable=self.selected_category, state='readonly')
+        self.category_combobox['values'] = ('ESAS', 'Mathematics', 'EEPS')
+        self.category_combobox.current(0)
+        self.category_combobox.pack(side=tk.TOP, padx=10, pady=3)
 
         # Create the fields
         question_label = ttk.Label(fields_frame, text='Question:')
